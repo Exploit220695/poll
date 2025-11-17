5d882dff2173f3a3b7a9cc5d105e48e166b5490a
Exploit220695 <136262681+Exploit220695@users.noreply.github.com>
2025-11-17 10:00:42 +0700

    Dashboard.tsx

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TrafficMonitor from "../components/TrafficMonitor";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <TrafficMonitor />
+    </div>
+  );
+}
