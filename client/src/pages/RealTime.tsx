856e1a7f50595c8de9b3e40387071b2ae6f82645
Exploit220695 <136262681+Exploit220695@users.noreply.github.com>
2025-11-17 10:02:54 +0700

    RealTime.tsx

---
+++ client/src/pages/RealTime.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DashboardGraph from "../components/DashboardGraph";
+
+export default function RealTime() {
+  return (
+    <div>
+      <DashboardGraph />
+    </div>
+  );
+}
