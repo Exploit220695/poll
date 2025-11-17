15b0e11173083e19d6e43c1858a68619aca87f2d
Exploit220695 <136262681+Exploit220695@users.noreply.github.com>
2025-11-17 08:28:05 +0700

    feat: realtime traffic, DDOS classification, mitigation, honeypot ports, UI (Bahasa Indonesia)

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { initTraffic } from "./integrate-traffic";
+import http from "http";
 
 const app = express();
 app.use(express.json());
@@ -12,8 +14,10 @@ app.use((req, res, next) => {
   let capturedJsonResponse: Record<string, any> | undefined = undefined;
 
   const originalResJson = res.json;
+  // @ts-ignore
   res.json = function (bodyJson, ...args) {
     capturedJsonResponse = bodyJson;
+    // @ts-ignore
     return originalResJson.apply(res, [bodyJson, ...args]);
   };
 
@@ -37,7 +41,11 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  const server = await registerRoutes(app);
+  // registerRoutes mengembalikan server HTTP di repositori ini
+  const server = (await registerRoutes(app)) as http.Server;
+
+  // Inisialisasi integrasi traffic (Socket.IO namespaces dan endpoint mitigasi)
+  initTraffic(app, server);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
@@ -47,20 +55,13 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
+  const port = parseInt(process.env.PORT || "5000", 10);
   server.listen({
     port,
     host: "0.0.0.0",
